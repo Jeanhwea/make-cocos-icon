d1d96ef8f702a2008e660bfaaa2d8160a008a7b7
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-02-24 10:39:16 +0800

    v0.3.4

--- src/__version__.py
+++ src/__version__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __app__ = "make-cocos-icon"
 __title__ = "Generate app icons for Cocos projects"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Jinghui Hu"
 __author_email__ = "hujinghui@buaa.edu.cn"
