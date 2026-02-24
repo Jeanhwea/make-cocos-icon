7c1e7d4f64f6c952a108a1be3785c337cb7b7290
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-02-24 08:42:11 +0800

    update: 添加日志配置

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+import logging
 import os
 import sys
-import logging
 
 # 添加工程根目录到 Python 路径
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -11,8 +11,10 @@ os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 os.environ["QT_API"] = "pyside6"
 DEBUGGING = os.environ.get("DD") not in (None, "", "0", "false", "no", "off")
 
+_APP_LOG_LEVEL = logging.DEBUG if DEBUGGING else logging.INFO
 
-logging.basicConfig(level=logging.INFO, format="%(message)s")
+logging.basicConfig(level=_APP_LOG_LEVEL, format="%(message)s")
+
 
 # 让未捕获的异常自动记录到日志
 def handle_uncaught_exception(exc_type, exc_value, exc_traceback):
