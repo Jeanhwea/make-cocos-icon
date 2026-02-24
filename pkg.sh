433ed6d42ea82282786d7113377bb6ccc9c5ab94
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-02-24 09:59:00 +0800

    update: 添加打包脚本

--- pkg.sh
+++ pkg.sh
@@ -4,8 +4,11 @@ set -eux
 APP_PKG=make-cocos-icon
 GIT_TAG=$(git describe --tags --always --dirty="+dev")
 OUT_DIR=output
+DST_DIR=dist
 OS_ARCH=$(uname -m)
 OS_NAME=$(uname -s)
+
+DMG_DIR=${OUT_DIR}/dmg
 ZIP_DIR=${OUT_DIR}
 ZIP_IMG=${APP_PKG}-${OS_NAME}-${OS_ARCH}-${GIT_TAG}.zip
 
@@ -16,5 +19,9 @@ rm -rf ${OUT_DIR}
 # source .venv/bin/activate
 uv run python -m nuitka main.py
 
-cd ${OUT_DIR}
-zip ${ZIP_IMG} ${APP_PKG}
+mv ${OUT_DIR}/${APP_PKG} ${DMG_DIR}
+hdiutil create -volname ${DMG_VOL} -srcfolder ${DMG_DIR} -ov -format UDZO ${DMG_IMG}
+mkdir -p ${DST_DIR}
+mv ${DMG_IMG} ${DST_DIR}
+
+rm -rf ${OUT_DIR}
